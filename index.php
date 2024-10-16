31cd982ae729954727ccbe7dd910fddae4bfbf3a
Ramesh92340 <rameshpilli1428@gmail.com>
2024-10-16 10:52:03 +0530

    slider removed

--- index.php
+++ index.php
@@ -176,9 +176,9 @@
 		<!-- <div class="slider-item" >
 				<img src="images/festival_sliders/ganesh_festivel.png" class="img-fluid" alt="">
 		</div> -->
-		<div class="slider-item" >
+		<!-- <div class="slider-item" >
 				<img src="images/festival_sliders/ARUNA DASARA.png" class="img-fluid" alt="">
-		</div>
+		</div> -->
 
 
 		<div class="slider-item  ">
